7dc4debda33a9d19e05c4cf92e9562aef56bf50c
AngMingZe <angmingze21@gmail.com>
2022-05-06 11:54:47 +0800

    prac 3 part 2 (Alert button)

--- app/src/main/java/com/example/s10203953/MainActivity.java
+++ app/src/main/java/com/example/s10203953/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.s10203953;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
             Button b1 = findViewById(R.id.follow);
             b1.setText("Unfollow");
         }
+        Intent receivingEnd = getIntent();
+        int message = receivingEnd.getIntExtra("rInte",66);
+        TextView hello = findViewById(R.id.hworld);
+        hello.setText("MAD + " + message);
+
     }
 
     public void followBut(View v){
@@ -49,4 +55,5 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"Unfollowed",Toast.LENGTH_SHORT).show();
         }
     }
+
 }
