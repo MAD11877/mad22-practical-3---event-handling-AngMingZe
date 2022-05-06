eea684903f968a6272de0c75c3df4ab5fbcfdba5
AngMingZe <angmingze21@gmail.com>
2022-05-06 11:02:28 +0800

    prac 3 part 1 (Toast)

--- app/src/main/java/com/example/s10203953/MainActivity.java
+++ app/src/main/java/com/example/s10203953/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public class User{
@@ -34,15 +35,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void anyName(View v){
+    public void followBut(View v){
         //Event handler
         Button b1 = findViewById(R.id.follow);
         String text = b1.getText().toString();//Text of follow button
         if (text == "Follow"){
             b1.setText("Unfollow");
+            Toast.makeText(getApplicationContext(),"Followed",Toast.LENGTH_SHORT).show();
+            // Toast.makeText(getApplicationContext(), <Text>, <Duration Visible>).show();
         }
         else{
             b1.setText("Follow");
+            Toast.makeText(getApplicationContext(),"Unfollowed",Toast.LENGTH_SHORT).show();
         }
     }
 }
